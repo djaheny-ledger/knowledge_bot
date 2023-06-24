171eefdde258275081453aee061fd96bf88d59a5
djaheny-ledger <132566767+djaheny-ledger@users.noreply.github.com>
2023-06-24 09:44:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,17 @@
 import os
 import uuid
 import json
-from typing import Callable, List, Union
-
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, make_response, redirect, jsonify
 from web3 import Web3
 from eth_account.messages import encode_defunct
-
 import pinecone
 import openai
-
-from langchain import LLMChain
-from langchain.agents import initialize_agent, load_tools, ZeroShotAgent, AgentExecutor, Tool, LLMSingleActionAgent, AgentOutputParser
-from langchain.prompts import BaseChatPromptTemplate
-from langchain.memory import ConversationBufferMemory, ChatMessageHistory
-from langchain.chains import ConversationChain, ConversationalRetrievalChain, RetrievalQA
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
-
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain import SerpAPIWrapper, LLMChain
-from typing import List, Union
-from langchain.schema import AgentAction, AgentFinish, HumanMessage
-from pathlib import Path
-
 import re
-
-
+from langchain.vectorstores import Pinecone
 
 load_dotenv()
-history = ChatMessageHistory()
+
 
 env_vars = [
     'OPENAI_API_KEY',
@@ -46,17 +25,10 @@ os.environ['WEB3_PROVIDER'] = f"https://polygon-mumbai.g.alchemy.com/v2/{os.envi
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 ORGANIZATION = os.getenv("ORGANIZATION")
+openai.api_key=os.environ['OPENAI_API_KEY']
 
 # Initialize web3
 web3 = Web3(Web3.HTTPProvider(os.environ['WEB3_PROVIDER']))
-
-#Initialize LLM
-llm=ChatOpenAI(
-    openai_api_key=os.environ['OPENAI_API_KEY'],
-    temperature=0.0,
-    model_name='gpt-3.5-turbo'
-    #model_name='gpt-4'
-)
 
 
 # Prepare augmented query
@@ -64,7 +36,6 @@ llm=ChatOpenAI(
 pinecone.init(api_key=os.environ['PINECONE_API_KEY'], enviroment=os.environ['PINECONE_ENVIRONMENT'])
 pinecone.whoami()
 #index_name = 'hc'
-#index_name = 'hctest'
 index_name = 'academyzd'
 index = pinecone.Index(index_name)
 
@@ -74,32 +45,25 @@ primer = """
 
 You are Samantha, a highly intelligent and helpful virtual assistant designed to support Ledger, a French cryptocurrency company led by CEO Pascal Gauthier. Your primary responsibility is to assist Ledger customer support agents by providing accurate answers to their questions. If a question is unclear or lacks detail, ask for more information instead of making assumptions. If you are unsure of an answer, be honest and seek clarification.
 
-Agents may ask about various Ledger products, including the Ledger Nano S (no battery, low storage), Nano X (Bluetooth, large storage, has a battery), Nano S Plus (large storage, no Bluetooth, no battery), Ledger Stax (unreleased), and Ledger Live.
-The official Ledger store is located at https://shop.ledger.com/. For authorized resellers, please visit https://www.ledger.com/reseller/ , do not modify or share any other links for these purposes. 
+Agents may ask about various Ledger products, including the Ledger Nano S (no battery, low storage), Nano X (Bluetooth, large storage, has a battery), Nano S Plus (large storage, no Bluetooth, no battery), Ledger Stax (unreleased), Ledger Recover and Ledger Live.
+The official Ledger store is located at https://shop.ledger.com/. The Ledger Recover White Paper is located at https://github.com/LedgerHQ/recover-whitepaper . For authorized resellers, please visit https://www.ledger.com/reseller/. Do not modify or share any other links for these purposes. 
 
-When agents inquire about tokens, crypto or coins supported in Ledger Live , it is crucial to strictly use the provided Crypto Asset List link to verify support. 
-
+When agents inquire about tokens, crypto or coins supported in Ledger Live , it is crucial to strictly recommend checking the Crypto Asset List link to verify support. 
 The link to the Crypto Asset List of supported crypto coins and tokens is: https://support.ledger.com/hc/en-us/articles/10479755500573?docs=true/. Do NOT provide any other links to the list.
-
-Here's a guide on using the Crypto Asset List:
-
-- The list is organized into bullet points with the following format: | network | token symbol | contract address | token name | countervalue
-- If a coin or token is on the list, it is supported in Ledger Live.
-- The list is not case-sensitive, so "BTC" and "btc" are treated as identical.
-- If a coin or token has "countervalues disabled" as its countervalue, it is supported in Ledger Live, but its value will not be displayed.
-- When a coin or token is supported, include information about the supported network.
-- ALWAYS provide the following link to the Crypto Asset List for users to find more information:  https://support.ledger.com/hc/en-us/articles/10479755500573?docs=true/ . Do not share any other links for this purpose.
-
 
 VERY IMPORTANT:
 
-- Always mention the source of your information (URL link) when providing answers, such as an official Help Center or Acedemy article or tutorial. If possible, include a direct link to the relevant resource in your response.
-- Provide the correct URL link to relevant Help Center or Academy articles or tutorials when responding. Do not share a link if uncertain of its accuracy.
+- If the query is not about Ledger products, disregard the CONTEXT. Respond courteously and invite any Ledger-related questions.
+- When answering a question, make sure to incorporate only the most relevant URL link from the CONTEXT in your response. 
+- Only share URLs that are explicitly mentioned within the CONTEXT.
+- If the provided CONTEXT does not include URLs, do not include any in your response.
+- Always format URL links in markdown format.
 - Direct users who want to learn more about Ledger products or compare devices to https://www.ledger.com/.
 - Updating or downloading Ledger Live must always be done via this link: https://www.ledger.com/ledger-live
 - Share this list for tips on keeping your recovery phrase safe: https://support.ledger.com/hc/en-us/articles/360005514233-How-to-keep-your-24-word-recovery-phrase-and-PIN-code-safe-?docs=true/
 
-Begin! Keep in mind that your primary objective is to assist them in effectively performing their duties.
+
+Begin!
 
 """
 
@@ -116,6 +80,7 @@ def authenticate(signature):
     message_hash = encode_defunct(text=message)
     signed_message = w3.eth.account.recover_message(message_hash, signature=signature)
     balance = int(contract.functions.balanceOf(signed_message).call())
+    print(balance)
     if balance > 0:
         token = uuid.uuid4().hex
         response = make_response(redirect('/gpt'))
@@ -133,10 +98,12 @@ def has_auth_token(request):
 @app.route("/")
 def home():
     return render_template("auth.html")
+    #return render_template('index.html')
 
 @app.route("/auth")
 def auth():
     signature = request.args.get("signature")
+    print(signature)
     response = authenticate(signature)
     return response
 
@@ -150,7 +117,6 @@ def gpt():
 @app.route('/api', methods=['POST'])
 def react_description():
     user_input = request.json.get('user_input')
-    query = user_input
     try:
 
         res_embed = openai.Embedding.create(
@@ -164,13 +130,16 @@ def react_description():
 
         contexts = [item['metadata']['text'] for item in res_query['matches']]
 
-        augmented_query = "\n\n---\n\n".join(contexts)+"\n\n-----\n\n"+ user_input + "? Please provide a comprehensive answer to the question, and make sure to incorporate relevant URL links from the previous context. NEVER enclose the links in parentheses. Do not share a link that's not explicitly included in the previous context."
+        augmented_query = "CONTEXT: " + "\n\n-----\n\n" + "\n\n---\n\n".join(contexts) + "\n\n-----\n\n"+ "QUESTION: " + "\n\n" + '"' + user_input + '" ' + "Please provide an answer to the question."
+
         print(augmented_query)
 
         res = openai.ChatCompletion.create(
-            
-            model="gpt-3.5-turbo",
-            #model="gpt-4",
+            temperature=0.0,
+            #model='gpt-4',
+            #model="gpt-3.5-turbo-16k",
+            model="gpt-3.5-turbo-0613",
+            #model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": primer},
                 {"role": "user", "content": augmented_query}
@@ -190,4 +159,4 @@ contract = web3.eth.contract(address=ADDRESS, abi=ABI)
 
 # Start the Flask app
 if __name__ == '__main__':
-    app.run(port=8000, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=1000, debug=False, use_reloader=False)
